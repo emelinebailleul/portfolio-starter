2a3b3cbd89049c9fb7842a4407550da94102831f
Bastien <bastienrbt@protonmail.com>
2017-12-25 12:31:25 +0100

    Remove URL middleware - Add transpath and async_image gems

--- Gemfile
+++ Gemfile
@@ -5,8 +5,12 @@ gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-livereload'
 
 gem 'middleman-minify-html'
+gem 'middleman-sprockets'
 gem 'middleman-imageoptim', :git => 'https://github.com/plasticine/middleman-imageoptim'
 gem "middleman-critical", "~>0.0.2"
+
+gem 'middleman-async-image'
+gem 'middleman-transpath'
 
 gem "middleman-sitemap", :git => "https://github.com/bastienrobert/middleman-sitemap"
 gem 'middleman-robots', '1.3.0'
--- config.rb
+++ config.rb
@@ -11,6 +11,12 @@ activate :directory_indexes
 activate :i18n, :mount_at_root => :fr
 # Using asset helpers
 activate :asset_hash
+# Using sprockets
+activate :sprockets
+# Loading images asynchronously
+activate :async_image
+# Middleman i18n can't convert page URL to another language. This is the solution.
+activate :transpath
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
@@ -41,10 +47,6 @@ Dir["helpers/*.rb"].each {|file| require file }
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 helpers ApplicationHelper
-
-# Middleman i18n can't convert page URL to another language. This is the solution.
-require "lib/url_middleware"
-helpers URLHelper
 
 # Middleman fails to reload on helpers edit. This is the solution.
 Dir['helpers/*'].each(&method(:load))
