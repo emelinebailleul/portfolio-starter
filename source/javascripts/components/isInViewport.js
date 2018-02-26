08bbf0148559bf2b4d2ff44001c8096dbbcd130e
emelinebailleul <emeline.bailleul@gmail.com>
2018-02-26 18:00:19 +0100

    fix isInViewport and add animations on project header

--- source/javascripts/components/isInViewport.js
+++ source/javascripts/components/isInViewport.js
@@ -1,38 +1,33 @@
 class isInViewport {
   constructor () {
 
-    var inViewport = function (el) {
-        var distance = el.getBoundingClientRect();
-        return (
-            distance.top >= 0 &&
-            distance.left >= 0 &&
-            distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    var inViewport = function (elem) {
+      var distance = elem.getBoundingClientRect();
+      return (
+        distance.top >= 0 &&
+        distance.left >= 0 &&
+        distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
-    };
+      };
 
-    var elems = document.querySelectorAll('[data-animate]');
-    var els = document.querySelectorAll('[data-animate-auto]');
 
-    // Animate on load
+
     window.onload = function() {
-      console.log('loaded1')
-      for (var el of els) {
-        if (inViewport(el) == true) {
-          console.log('loaded2')
-          el.classList.add('visible');
-        }
+      //document.getElementById('sounds-in-bordeaux').classList.add('visible')
+      var elems = document.querySelectorAll('[data-animate-auto]')
+      for (var elem of elems) {
+        elem.classList.add('visible')
       }
     }
 
-    // Animate on scroll to create fade in effect
     document.onscroll = function() {
-      console.log('scroll')
+      var elems = document.querySelectorAll('[data-animate]')
       for (var elem of elems) {
         if (inViewport(elem)) {
-          elem.classList.add('visible');
+          elem.classList.add('visible')
         } else {
-          elem.classList.remove('visible');
+          elem.classList.remove('visible')
         }
       }
     }
