0f683f4e72969671ab882b520329d5a3ba745f6f
emelinebailleul <emeline.bailleul@gmail.com>
2019-06-25 20:34:10 +0200

    remove imageoptim from config.rb

--- config.rb
+++ config.rb
@@ -87,10 +87,6 @@ configure :build do
   activate :minify_html
   activate :minify_css
   activate :minify_javascript
-  activate :imageoptim do |options|
-    options.manifest = true
-    options.pngout = false
-  end
   activate :gzip
   activate :critical
 
